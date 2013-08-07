f7f874f77b4c8d0e56bae2a99787997f70de5722
airtonjal <airtonjal@gmail.com>
2013-08-07 16:23:31 -0300

    - Post example

---
+++ post.lua
@@ -0,0 +1,9 @@
+--return "http://127.0.0.1.:8000/Service/Calculator.svc/Add",
+return "http://127.0.0.1.:8000/Service/Calculator.svc/Subtract",
+--return "http://127.0.0.1.:8000/Service/Calculator.svc/Divide",
+--return "http://127.0.0.1.:8000/Service/Calculator.svc/Multiply",
+--return "http://airton-PC:49307/Service/Calculator.svc/Add", 
+
+{ N1 = 24 , N2 =  10 }
+
+, nil
