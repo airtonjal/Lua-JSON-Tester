53807ed4e628b8f2ee7c592dd20fcda84f6b237e
airton.liborio <airton.liborio@webradar.com>
2014-05-21 11:12:54 -0300

    - Adding simple imsi test - Adding cdr configuration

---
+++ config/cdr.lua
@@ -0,0 +1,14 @@
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--[[ Change the values bellow according to your server configuration ]]--
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+
+verbose       = true  -- Just local output
+
+httpPort      = 8080  -- Outgoing http port
+httpsPort     = 443 -- Outgoing https port
+
+SERVER        = "10.8.0.214"
+PROTOCOL      = PROTOCOLS.HTTP
+PORT          = httpPort
---
+++ tests/cdr/imsi.lua
@@ -0,0 +1,26 @@
+require "config.current"
+require "senderFunction"
+require "utils"
+
+-- Request table. Each key is a method name and each value is the object to convert to json data on the POST request
+local imsis = {
+  "706030011334680"
+}
+
+local path = "cdr-rest/huawei/tigo/search/imsi/%s"
+
+function test()
+  -- Invoke services with POST requests
+  for _, imsi in pairs(imsis) do
+  --  printInfo("Testing " .. service:upper() .. " service with HTTP")
+  --  request(serverAddress, 4430,  PROTOCOLS.HTTP,  METHOD.POST, CONTENTS.JSON, path:format(service), CONTENTS.JSON, data)
+    printInfo("Testing " .. imsi .. " service with HTTP")
+    requestAndPrint(SERVER, PORT, PROTOCOL, METHOD.GET, CONTENTS.JSON, path:format(imsi), CONTENTS.JSON, nil, true)
+  end
+end
+
+for i = 1, arg[1] or 1 do
+  test()
+end
+
+print()
