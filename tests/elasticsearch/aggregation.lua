d137e7f4a6423e7f43e09b29b8cf0ff2f365607c
root <root@ES02DEV.wradar.br>
2015-03-06 17:25:58 -0300

    - Adding queries

--- tests/elasticsearch/aggregation.lua
+++ tests/elasticsearch/aggregation.lua
@@ -6,19 +6,40 @@ verbose = true
 
 local broker = Broker ("ampere.poc.wr01.wradar.br", 9200, PROTOCOLS.HTTP)
 
-broker:requestAndPrint(METHOD.GET, CONTENTS.JSON, "", CONTENTS.QUERY, {})
+broker:requestAndPrint(METHOD.GET, CONTENTS.JSON, "", CONTENTS.JSON, {})
 
 -- Request table. Each key is a method name and each value is the object to convert to json data on the POST request
 local posts = {
   -- SERVICE               PARAMETERS
-  search = { query = {  }   },
+  pchr = { 
+    query = { 
+      bool = { 
+        must = { 
+          { match_all = { } } 
+        }
+      }
+    },
+    from = 0,
+    size = 10
+  },
 }
 
-local path = "%s"
+local tacs = {
+  pchr = {
+    size = 0,
+    filter = {
+      exists = {
+        field = "Call.Phone.tac"
+      }
+    }
+  }
+}
+
+local path = "%s/_search"
 -- Invoke services with POST requests
-for service, data in pairs(posts) do
+for service, data in pairs(tacs) do
   printInfo("Testing " .. service:upper() .. " with HTTP")
-  broker:requestAndPrint(METHOD.POST, CONTENTS.JSON, path:format(service), CONTENTS.QUERY, data)
+  broker:requestAndPrint(METHOD.POST, CONTENTS.JSON, path:format(service), CONTENTS.JSON, data)
 end
 
-rint()
+print()
